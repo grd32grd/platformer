c18e6b433b28a85e75d7471873699d38a3749be6
guledmoh12 <guledmo12@gmail.com>
2021-07-07 14:56:54 -0400

    Adding backgrounf image

--- platformer.py
+++ platformer.py
@@ -1,6 +1,8 @@
 import os
 os.environ['PYGAME_HIDE_SUPPORT_PROMPT'] = "hide"
 import pygame
+from pygame import *
+import pygame.image
 
 pygame.init()
 SCREENWIDTH = 1000
@@ -11,6 +13,8 @@ WHITE = (255,255,255)
 screen = pygame.display.set_mode((SCREENWIDTH, SCREENHEIGHT))
 pygame.display.set_caption("Platformer")
 
+#Background
+bg1 = pygame.image.load("bg1.png").convert()
 pygame.draw.rect(screen, WHITE , (400, 400, 20, 20),0)
 screen.fill(WHITE)
 
@@ -18,8 +22,11 @@ pygame.display.update()
 
 running = True
 while running:
+
+    screen.blit(bg1,(0,0))
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
+    pygame.display.update()
 pygame.quit()
