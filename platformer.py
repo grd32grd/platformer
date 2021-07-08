543d5532af978eae7a8414a63033a9b1a9ec1a70
guledmoh12 <guledmo12@gmail.com>
2021-07-08 00:27:44 -0400

    Setup World & Tiles

--- platformer.py
+++ platformer.py
@@ -1,32 +1,109 @@
+#PYGAME SETUP
 import os
 os.environ['PYGAME_HIDE_SUPPORT_PROMPT'] = "hide"
 import pygame
 from pygame import *
-import pygame.image
 
 pygame.init()
-SCREENWIDTH = 1000
-SCREENHEIGHT = 1000
 
-#Colors
-WHITE = (255,255,255)
-screen = pygame.display.set_mode((SCREENWIDTH, SCREENHEIGHT))
+#CONSTANT VARIABLES
+SCREEN_WIDTH = 1000
+SCREEN_HEIGHT = 1000
+TILE_SIZE = 50
+TILES_AMOUNT = 20
+
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+GREEN = (0, 255, 0)
+RED = (255, 0, 0)
+BLUE = (0, 0, 255)
+
+#OTHER VARIABLES
+world_data = [
+[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 
+[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],  
+[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 
+[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 
+[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 
+[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 
+[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 
+[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 
+[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 
+[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 
+[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 
+[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 
+[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 
+[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 
+[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 
+[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 
+[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 
+[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 
+[2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2],
+[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+]
+
+#CLASSES
+class World():
+	def __init__(self, data):
+		self.tile_list = []
+
+		dirt = pygame.image.load('Assets/brown.png')
+		grass = pygame.image.load('Assets/green.png')
+
+		row_count = 0
+		for row in data:
+			col_count = 0
+			for tile in row:
+				#Dirt Tiles
+				if tile == 1:
+					img = pygame.transform.scale(dirt, (TILE_SIZE, TILE_SIZE))
+					img_rect = img.get_rect()
+					img_rect.x = col_count * TILE_SIZE
+					img_rect.y = row_count * TILE_SIZE
+					tile = (img, img_rect)
+					self.tile_list.append(tile)
+				#Grass Tiles	
+				elif tile == 2:
+					img = pygame.transform.scale(grass, (TILE_SIZE, TILE_SIZE))
+					img_rect = img.get_rect()
+					img_rect.x = col_count * TILE_SIZE
+					img_rect.y = row_count * TILE_SIZE
+					tile = (img, img_rect)
+					self.tile_list.append(tile)
+				col_count += 1
+			row_count += 1
+
+	def draw(self):
+		for tile in self.tile_list:
+			screen.blit(tile[0], tile[1])
+
+#OTHER FUNCTIONS
+def grid():
+	for line in range(0, TILES_AMOUNT):
+		pygame.draw.line(screen, WHITE, (0, line * TILE_SIZE), (SCREEN_WIDTH, line * TILE_SIZE))
+		pygame.draw.line(screen, WHITE, (line * TILE_SIZE, 0), (line * TILE_SIZE, SCREEN_HEIGHT))
+
+
+   
+#SCREEN SETUP
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption("Platformer")
+world = World(world_data)
+bg = pygame.image.load('Assets/lightblue.jpg').convert()
 
-#Background
-bg1 = pygame.image.load("bg1.png").convert()
-pygame.draw.rect(screen, WHITE , (400, 400, 20, 20),0)
-screen.fill(WHITE)
 
-pygame.display.update()
+#GAME
+game = True
+while game:
 
-running = True
-while running:
+    screen.blit(bg,(0,0))
 
-    screen.blit(bg1,(0,0))
+    world.draw()
+    grid()
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            running = False
+            game = False
 
     pygame.display.update()
 pygame.quit()
